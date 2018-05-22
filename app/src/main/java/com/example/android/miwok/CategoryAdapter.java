9152970fb9eb0dad804a24227ad6725dd5ddd228
kshntn <kshntn@gmail.com>
2018-05-22 02:58:17 +0200

    final

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,12 +1,28 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
-    public CategoryAdapter(FragmentManager fm) {
+    private Context mContext;
+    public CategoryAdapter(Context context,FragmentManager fm) {
         super(fm);
+        mContext =context;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0)
+            return mContext.getString(R.string.category_numbers);
+        else if (position == 1)
+            return mContext.getString(R.string.category_family);
+        else if (position == 2)
+            return mContext.getString(R.string.category_colors);
+        else if (position == 3)
+            return mContext.getString(R.string.category_phrases);
+        return null;
     }
 
     @Override
